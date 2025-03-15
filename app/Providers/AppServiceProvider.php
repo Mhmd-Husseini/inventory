2980092ed659a9a57be1a0c8a716f0cb5da445c4
Mhmd Husseini <mhmd.husseinii@gmail.com>
2025-03-15 15:32:45 +0200

    create migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+        
+        \Illuminate\Database\MySqlConnection::resolverFor('mysql', function ($connection, $database, $prefix, $config) {
+            $connection = new \Illuminate\Database\MySqlConnection($connection, $database, $prefix, $config);
+            $connection->setSchemaGrammar(new \Illuminate\Database\Schema\Grammars\MySqlGrammar);
+            $connection->setQueryGrammar(new \Illuminate\Database\Query\Grammars\MySqlGrammar);
+            $connection->setPostProcessor(new \Illuminate\Database\Query\Processors\MySqlProcessor);
+            $connection->statement('SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci');
+            return $connection;
+        });
     }
 }
